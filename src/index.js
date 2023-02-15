2432fde83ac0a50b09d8b0f1822b5b11a281f8cb
Nikolay Lapshin <nlapshin1989@gmail.com>
2023-02-15 22:38:35 +0300

    Added deployment and service

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+const os = require('os');
+
 const express = require('express');
 const app = express();
 
 let count = 0;
 
 app.get('/count', (req, res) => {
-  return res.json({ count: ++count });
+  return res.json({ count: ++count, hostname: os.hostname() });
 })
 
 app.listen(4000);
 
 console.log('Server started on 4000 port');
+console.log('Server hostname:', os.hostname())
